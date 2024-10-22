35f080af382cea3466a294355301aada0c3c9613
Pasumarthi Ashik <pasumarthiashik88874@gmail.com>
2024-10-22 18:25:33 +0530

    new changes are updated

--- personalportfolio/src/App.js
+++ personalportfolio/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import PreLoader from './PreLoader';
 import HomePage from './HomePage';
-import FeedbackPage from './FeedBack_Grid';
+// import FeedbackPage from './FeedBack_Grid'; this is done using useState hook
+import Practise from './Feedback_Grid_useReducer'; // this is done using useReducer hook
 // import { useState, useEffect } from 'react';
 function App() {
   // let [isLoading, setIsLoading] = useState(true);
@@ -26,7 +27,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/1" element={ <HomePage /> } />
-          <Route path="/7" element={ <FeedbackPage /> } /> {/* Route for FeedbackPage */ }
+          {/* <Route path="/7" element={ <FeedbackPage /> } /> {/* Route for FeedbackPage */ } */}
+          <Route path="/7" element = {<Practise/>} />
         </Routes>
       </Router>
 
